5b81024ed72b8c4c3b3f9eedb0e84531b7b49399
Mahmood Mohammed Seoud <mse@peoplenet.dk>
2025-10-19 01:51:53 +0200

    Fix: Import all module dependencies in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@
  * @license MIT
  */
 
-// Import main class
+// Import all modules in correct dependency order
+import './constants.js';
+import './layout-calculator.js';
+import './config-parser.js';
+import './base-ring.js';
+import './calendar-ring.js';
+import './header-ring.js';
+import './data-ring.js';
+import './ring-factory.js';
+import './circalify-core.js';
 import AnnualWheel from './circalify.js';
 
 // Export as default
